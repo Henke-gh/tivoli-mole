aa3561fcc2d48e112d21e038c042c2f41630bc23
Josefinx <josefin.wetterberg@gmail.com>
2025-05-16 13:47:02 +0200

    started styling header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <p className="logo">Guac-a-mole</p>
-      <img src="./example_todo.png" alt="example" className="example" />
+      <img className="logo-img" src="../../../public/gouacBowl.svg" alt="example" />
     </header>
   );
 }
