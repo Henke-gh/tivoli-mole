59c4e6791d896e0a2f2cbeb90be9da4f85d0179f
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-05-21 13:37:52 +0200

    work on JWT and payment

---
+++ src/config/gameConfig.ts
@@ -0,0 +1,11 @@
+import { GameConfig } from "../types/types";
+
+export const GAME_CONFIG: GameConfig = {
+  AMUSEMENT_ID: 9, // Default value, override with env if available
+  GROUP_ID: 2,
+  COST: 2.0,
+  CURRENCY: "EUR",
+  STAMP_ID: 7, // Default value, override with env if available
+};
+
+//console.log("Game configuration:", GAME_CONFIG);
