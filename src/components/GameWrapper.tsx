1d583abf015672da023cf8d3feb1e52d2f039644
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-05-25 13:01:29 +0200

    jwtdisplay disabled

--- src/components/GameWrapper.tsx
+++ src/components/GameWrapper.tsx
@@ -1,25 +1,25 @@
 "use client";
 
-import { useGameContext } from '../components/GameContext';
-import Startgame from '../components/Startgame/Startgame'; 
-import Gameover from '../components/Gameover/Gameover';
-import MoleBoard from '../components/MoleBoard/MoleBoard';
-import styles from '../app/page.module.css'
-import JwtDisplay from './JwtDisplay';
+import { useGameContext } from "../components/GameContext";
+import Startgame from "../components/Startgame/Startgame";
+import Gameover from "../components/Gameover/Gameover";
+import MoleBoard from "../components/MoleBoard/MoleBoard";
+import styles from "../app/page.module.css";
+/* import JwtDisplay from './JwtDisplay'; */
 
 const GameContent: React.FC = () => {
-  const { 
-    gameStarted, 
-    gameOver, 
+  const {
+    gameStarted,
+    gameOver,
     score,
-    handleRestartGame, 
-    handleGameOver, 
-    handleScoreUpdate 
+    handleRestartGame,
+    handleGameOver,
+    handleScoreUpdate,
   } = useGameContext();
 
   return (
     <div className={styles.page}>
-        <JwtDisplay />
+      {/*    <JwtDisplay /> */}
       {!gameStarted ? (
         <Startgame />
       ) : gameOver ? (
